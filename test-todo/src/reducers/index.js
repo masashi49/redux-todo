165e491e19cbcaaf21c65d3d55969f004fdd6070
yamada <yamada@reas.jp>
2020-01-08 14:17:43 +0900

    https://qiita.com/xkumiyu/items/9dfe51d2bcb3bdb06da3

--- test-todo/src/reducers/index.js
+++ test-todo/src/reducers/index.js
@@ -1,5 +1,11 @@
+/**
+ * 複数のreducersを結合。
+ * storeに食わせるreducerはindex.jsにcombineReducersを使ってまとめておく。
+ * こうすることでcreateStoreに食わせるjsはこの1つで済む。
+ */
+
 import { combineReducers } from 'redux'
 import todos from './todos'
 
-const todoApp = combineReducers( { todos } )
+const todoApp = combineReducers({ todos }) // todo reducersをcombineReducersに食わせて返す
 export default todoApp
