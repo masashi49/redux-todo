a789655de8b2fa385e77b8051b207e78e1b0d036
yamada <yamada@reas.jp>
2020-01-22 13:39:39 +0900

    分割代入をやめてみる

--- test-todo/src/components/TodoList.js
+++ test-todo/src/components/TodoList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Todo from './Todo';
 import PropTypes from 'prop-types'; //reactの中からではなく、prop-typesという　別のもので呼び出す必要がある
 
-const TodoList = ({ todos }) => ( //mapStateToPropsの内容をtodosとして受け取る { todos: state.todos }
+const TodoList = (todos) => ( //mapStateToPropsの内容をtodosとして受け取る { todos: state.todos }
   <ul>
-    {todos.map((todo) =>
+    {todos.todos.map((todo) =>
       <Todo
         key={todo.id}
         {...todo} //分割代入で子コンポーネントへ渡す
